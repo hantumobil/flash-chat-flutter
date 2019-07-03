66c7833520de673b659fb33cfa08adb6d7ed227c
windo <herwindo.artono@go-jek.com>
2019-07-03 21:56:45 +0700

    feat: reversed and ux chat

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -177,7 +177,7 @@ class MessagesStream extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data.documents;
+        final messages = snapshot.data.documents.reversed;
 
         List<MessageBubble> messageWidgets = [];
         for (var message in messages) {
@@ -192,6 +192,7 @@ class MessagesStream extends StatelessWidget {
 
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(
               horizontal: 10.0,
               vertical: 20.0,
